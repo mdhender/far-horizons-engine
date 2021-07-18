1dc59b83c2447cd204e64608fead16749a9f6093
Michael D Henderson <mdhender@mdhender.com>
2021-07-18 04:40:16 +0000

    added mkjson

--- engine/src/mkjson.c
+++ engine/src/mkjson.c
@@ -38,12 +38,17 @@ isset(int species_number, int32_t set[]) {
 int
 main(int argc, char *argv[]) {
     FILE *fp;
+    int num_species;
 
     /* Get all binary data. */
     get_galaxy_data();
     get_star_data();
     get_planet_data();
     get_species_data();
+
+    num_stars = 1;
+    num_planets = 1;
+    num_species = galaxy.num_species;
 
     fp = fopen("galaxy.json", "w");
     if (fp == NULL) {
@@ -62,7 +67,6 @@ main(int argc, char *argv[]) {
     fprintf(fp, "\t},\n");
 
     fprintf(fp, "\t\"stars\": [\n");
-    num_stars = 1;
     for (int i = 0; i < num_stars; i++) {
         struct star_data *star = star_base + i;
         fprintf(fp, "\t\t{\n");
@@ -128,6 +132,84 @@ main(int argc, char *argv[]) {
     fprintf(fp, "\t],\n");
 
     fprintf(fp, "\t\"species\": [\n");
+    for (int i = 0; i < num_species; i++) {
+        int species_number = i + 1;
+        struct species_data *species     = &spec_data[species_number - 1];
+        struct nampla_data  *nampla_base = namp_data[species_number - 1];
+        struct ship_data    *ship_base   = ship_data[species_number - 1];
+        fprintf(fp, "\t\t{\n");
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "id", species_number);
+        fprintf(fp, "\t\t\t\"%s\": [\n", "ally");
+        for (int j = 0; j < NUM_CONTACT_WORDS; j++) {
+            if (species->ally[j] != 0) {
+                char *vsep = "";
+                for (int sp = 1; sp <= MAX_SPECIES; sp++) {
+                    if (isset(sp, species->ally)) {
+                        fprintf(fp, "%s%d", vsep, sp);
+                        vsep = ", ";
+                    }
+                }
+                break;
+            }
+        }
+        fprintf(fp, "],\n");
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "auto_orders", species->auto_orders);
+        fprintf(fp, "\t\t\t\"%s\": [\n", "contact");
+        for (int j = 0; j < NUM_CONTACT_WORDS; j++) {
+            if (species->contact[j] != 0) {
+                char *vsep = "";
+                for (int sp = 1; sp <= MAX_SPECIES; sp++) {
+                    if (isset(sp, species->contact)) {
+                        fprintf(fp, "%s%d", vsep, sp);
+                        vsep = ", ";
+                    }
+                }
+                break;
+            }
+        }
+        fprintf(fp, "],\n");
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "econ_units", species->econ_units);
+        fprintf(fp, "\t\t\t\"%s\": [\n", "enemy");
+        for (int j = 0; j < NUM_CONTACT_WORDS; j++) {
+            if (species->enemy[j] != 0) {
+                char *vsep = "";
+                for (int sp = 1; sp <= MAX_SPECIES; sp++) {
+                    if (isset(sp, species->enemy)) {
+                        fprintf(fp, "%s%d", vsep, sp);
+                        vsep = ", ";
+                    }
+                }
+                break;
+            }
+        }
+        fprintf(fp, "],\n");
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "fleet_cost", species->fleet_cost);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "fleet_percent_cost", species->fleet_percent_cost);
+        fprintf(fp, "\t\t\t\"%s\": \"%s\",\n", "govt_name", species->govt_name);
+        fprintf(fp, "\t\t\t\"%s\": \"%s\",\n", "govt_type", species->govt_type);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "hp_original_base", species->hp_original_base);
+        fprintf(fp, "\t\t\t\"%s\": [%d,%d,%d,%d,%d,%d],\n", "init_tech_level", species->init_tech_level[0], species->init_tech_level[1], species->init_tech_level[2], species->init_tech_level[3], species->init_tech_level[4], species->init_tech_level[5]);
+        fprintf(fp, "\t\t\t\"%s\": \"%s\",\n", "name", species->name);
+        fprintf(fp, "\t\t\t\"%s\": [%d,%d,%d,%d,%d,%d],\n", "neutral_gas", species->neutral_gas[0], species->neutral_gas[1], species->neutral_gas[2], species->neutral_gas[3], species->neutral_gas[4], species->neutral_gas[5]);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "num_namplas", species->num_namplas);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "num_ships", species->num_ships);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "pn", species->pn);
+        fprintf(fp, "\t\t\t\"%s\": [%d,%d,%d,%d,%d,%d],\n", "poison_gas", species->poison_gas[0], species->poison_gas[1], species->poison_gas[2], species->poison_gas[3], species->poison_gas[4], species->poison_gas[5]);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "required_gas", species->required_gas);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "required_gas_max", species->required_gas_max);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "required_gas_min", species->required_gas_min);
+        fprintf(fp, "\t\t\t\"%s\": [%d,%d,%d,%d,%d,%d],\n", "tech_eps", species->tech_eps[0], species->tech_eps[1], species->tech_eps[2], species->tech_eps[3], species->tech_eps[4], species->tech_eps[5]);
+        fprintf(fp, "\t\t\t\"%s\": [%d,%d,%d,%d,%d,%d],\n", "tech_knowledge", species->tech_knowledge[0], species->tech_knowledge[1], species->tech_knowledge[2], species->tech_knowledge[3], species->tech_knowledge[4], species->tech_knowledge[5]);
+        fprintf(fp, "\t\t\t\"%s\": [%d,%d,%d,%d,%d,%d],\n", "tech_level", species->tech_level[0], species->tech_level[1], species->tech_level[2], species->tech_level[3], species->tech_level[4], species->tech_level[5]);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "x", species->x);
+        fprintf(fp, "\t\t\t\"%s\": %d,\n", "y", species->y);
+        fprintf(fp, "\t\t\t\"%s\": %d\n", "z", species->z);
+        if (i + 1 < num_species) {
+            fprintf(fp, "\t\t},\n");
+        } else {
+            fprintf(fp, "\t\t}\n");
+        }
+    }
     fprintf(fp, "\t],\n");
 
     fprintf(fp, "\t\"locations\": [\n");
@@ -142,5 +224,6 @@ main(int argc, char *argv[]) {
 
     return 0;
 }
+
 
 
